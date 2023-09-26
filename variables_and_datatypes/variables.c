455b3f39be7885fc4e096573abfaaaef27a00afd
swarupsahu08 <swarup8sahu@gmail.com>
2023-09-27 00:34:15 +0530

    modified:   variables_and_datatypes/variables.c  documentation 	modified:   variables_and_datatypes/variables.exe documentation

--- variables_and_datatypes/variables.c
+++ variables_and_datatypes/variables.c
@@ -3,15 +3,15 @@ int main(){
     //Variables are the names given to the container for memory and the address of the memory.For different types of data takes different size of memory 
 
     // sizeof() gives the size of all the data types
-    int a;
+    int a;  //Integeer
     printf("The size of the int is %d\n",sizeof(a));
-    float b;
+    float b;  //decimal point
     printf("The size of the float is %d\n",sizeof(b));
-    double c;
+    double c; //decimal point precision to morethan one digit more than the decimal point
     printf("The size of the double is %d\n",sizeof(c));
-    long int d;
+    long int d; //long integer 
     printf("The size of the long int is %d\n",sizeof(d));
-    short int e;
+    short int e; //short  int is used to shorten the size of int
     printf("The size of the short int is %d\n",sizeof(e));
     char f;
     printf("The size of the char is %d\n",sizeof(f));
